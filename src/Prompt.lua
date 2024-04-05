6ea32197f551d9956e21ca6a97d64dc559fe04e9
oezingle <oezingle@gmail.com>
2024-04-05 01:28:26 -0700

    Removed mouse event handler logic

--- src/Prompt.lua
+++ src/Prompt.lua
@@ -2,92 +2,20 @@ local class        = require("lib.30log")
 local string_split = require("src.polyfill.string_split")
 local list_reduce  = require("src.polyfill.list_reduce")
 local parse_key    = require("src.parse_key")
-local get_os       = require("src.get_os")
 local icons        = require("src.icons")
 local stringify    = require("src.stringify")
-
-local lgi          = require("lgi")
-local GLib         = lgi.require("GLib")
 
 -- TODO probably a good idea to allow userdata with pairs/ipairs to be inspected
 
 ---@class LuaInspect.Prompt : Log.BaseFunctions
 ---@field expand boolean
----@field mainloop unknown
+---@field stopped boolean
 ---@field identifier string
----@field fancy { os: "unix", tty: string, chars: {} }?
 ---@field path string[]
 ---@field object any
 ---
 ---@operator call:LuaInspect.Prompt
 local Prompt       = class("Prompt")
-
--- determine if fancy terminal can be done using stty.
-function Prompt:detect_can_fancy()
-    local systemos = get_os()
-
-    -- TODO does mac os support fancy xterm shit? also fancy mode should be disable-able.
-    -- system is unix
-    if systemos == "linux" or systemos == "mac-os" then
-        local handle = io.popen("which stty", "r")
-
-        -- system has stty
-        if handle and handle:read("l") then
-            local tty = self:stty("-echo", "cbreak")
-
-            -- stty can save
-            if tty then
-                self.fancy = { os = "unix", tty = tty, chars = {} }
-
-                --[[
-                    Enable mouse clicking
-                        - https://unix.stackexchange.com/questions/418901/how-some-applications-accept-mouse-click-in-bash-over-ssh
-                        - https://stackoverflow.com/questions/5966903/how-to-get-mousemove-and-mouseclick-in-bash
-                        - https://www.x.org/docs/xterm/ctlseqs.pdf
-                    TODO \027[?1001h should allow highlight reporting but my terminals seem to not support it.
-                ]]
-
-                io.output():write("\027[?1000h")
-                io.output():write("\027[?1006h")
-                io.output():write("\027[?1015h")
-            end
-        end
-    end
-end
-
-function Prompt:fancy_cleanup()
-    if self.fancy then
-        -- restore tty state
-        self:stty(self.fancy.tty)
-
-        io.output():write("\027[?1000l")
-        io.output():write("\027[?1006l")
-        io.output():write("\027[?1015l")
-
-        -- TODO fixme only print this if clean exit
-        print()
-
-        self.fancy = nil
-    end
-end
-
--- http://lua-users.org/lists/lua-l/2012-09/msg00360.html
----@param ... string
----@return string|nil
-function Prompt:stty(...)
-    local ok, p = pcall(io.popen, "stty -g")
-
-    if not ok or not p then return nil end
-
-    local state = p:read()
-    p:close()
-
-    if state and #... then
-        os.execute(table.concat({ "stty", ... }, " "))
-    end
-
-    return state
-end
 
 function Prompt:init(obj)
     self.expand = false
@@ -109,26 +37,6 @@ function Prompt:init(obj)
         until name == nil
     end
 
-    self.mainloop = GLib.MainLoop()
-
-    -- TODO FIXME fancy mode is disabled for now as stringify needs work.
-    -- self:detect_can_fancy()
-
-    GLib.idle_add(GLib.PRIORITY_DEFAULT, function()
-        local ok = xpcall(function()
-            self:wait()
-        end, function(err)
-            self:fancy_cleanup()
-
-            local trace = debug.traceback(err)
-            print(trace)
-
-            self:command_exit()
-        end)
-
-        return ok
-    end)
-
     self.path = {}
 
     self.object = obj
@@ -146,9 +54,6 @@ function Prompt:print()
     self:clear()
 
     -- Top bar
-    if self.fancy then
-        io.output():write(icons.ARROW_UP, " ")
-    end
     print(string.format("[%s] %s", self.identifier, table.concat(self.path, ".")))
 
     local obj = self:recurse_path()
@@ -183,9 +88,7 @@ function Prompt:command_expand()
 end
 
 function Prompt:command_exit()
-    self.mainloop:quit()
-
-    self:fancy_cleanup()
+    self.stopped = true
 end
 
 --- Move down the path given the key to a child
@@ -296,94 +199,27 @@ function Prompt:prompt()
     io.output():write("inspect: ")
 end
 
---- Detect mouse releases based on the 3 DIFFERENT formats terminals might return.
---- XTerm interrupts are terrible.
---- TODO what happens when scroll
-function Prompt:fancy_detect_mouse_release()
-    local input = table.concat(self.fancy.chars)
-
-    -- TODO perhaps match "exit\n" ?
-
-    local pattern_1000 = "\027%[M#(.)(.)$"
-    if input:match(pattern_1000) then
-        local x_str, y_str = input:match(pattern_1000)
-
-        local x_off, y_off = string.byte(x_str), string.byte(y_str)
-        local x, y = x_off - 32, y_off - 32
-
-        self.fancy.chars = {}
-
-        return x, y
-    end
-
-    local pattern_1000_1006 = "\027%[<0;(%d+);(%d+)m"
-    if input:match(pattern_1000_1006) then
-        local x_str, y_str = input:match(pattern_1000_1006)
-        local x, y = tonumber(x_str), tonumber(y_str)
-
-        self.fancy.chars = {}
-
-        return x, y
-    end
-
-    local pattern_1000_1015 = "\027%[35;(%d+);(%d+)M"
-    if input:match(pattern_1000_1015) then
-        local x_str, y_str = input:match(pattern_1000_1015)
-        local x, y = tonumber(x_str), tonumber(y_str)
-
-        self.fancy.chars = {}
-
-        return x, y
-    end
-
-    return nil, nil
-end
-
-function Prompt:fancy_handle_click()
-    local x, y = self:fancy_detect_mouse_release()
-
-    if not x or not y then
-        return
-    end
-
-    if x == 1 and y == 2 then
-        self:command_expand()
-    end
-
-    if x == 1 and y == 1 then
-        self:command_up()
-    end
-
-    -- self:warn_user(tostring(x) .. " " .. tostring(y))
-    -- TODO let table keys register their positions somehow - both in flat and expanded mode.
-
-    self:prompt()
-end
-
 function Prompt:wait()
-    if self.fancy then
-        local char = io.input():read(1)
+    self:prompt()
 
-        -- local keycode = string.byte(char)
-        -- print(char, keycode)
+    local cmd = io.input():read("l")
 
-
-        table.insert(self.fancy.chars, char)
-
-        self:fancy_handle_click()
-    else
-        self:prompt()
-
-        local cmd = io.input():read("l")
-
-        self:command(cmd)
-    end
+    self:command(cmd)
 end
 
 function Prompt:start()
-    self:prompt()
+    self.stopped = false
 
-    self.mainloop:run()
+    repeat
+        local ok = xpcall(function()
+            self:wait()
+        end, function(err)
+            local trace = debug.traceback(err)
+            print(trace)
+
+            self:command_exit()
+        end)
+    until not ok or self.stopped
 end
 
 return Prompt
